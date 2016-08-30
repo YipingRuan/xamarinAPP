eb1b566115f2bf0426659f2d3a00cb682ea3e3e3
admin <admin@DESKTOP-OPTNV4E>
2016-08-31 01:51:03 +0800

    444444444

---
+++ LYL.Controls/Controls.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace LYL.Controls
+{
+    public static class Controls
+    {
+        public static void Init()
+        {
+
+        }
+    }
+}
---
+++ LYL.Controls/count.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms.Xaml;
+
+namespace LYL.Controls
+{
+    public class count : IMarkupExtension
+    {
+        public Int32 i { get; set; }
+        public Int32 j { get; set; }
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return "count="+(i+j).ToString();
+        }
+    }
+}
